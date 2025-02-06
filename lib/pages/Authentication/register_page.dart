1ac5954a80673a9d4d69909a8c9a0a531253783d
nurislamilyasov <nurikileasov0000@gmail.com>
2025-02-06 10:38:25 +0500

    register

--- lib/pages/Authentication/register_page.dart
+++ lib/pages/Authentication/register_page.dart
@@ -5,6 +5,8 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Colors.white70,
+    );
   }
 }
