cfffe07fd26a753c98ad0ce02ac5de7d69790e4a
Bekarys <bekaryserezep05@gmail.com>
2025-02-06 10:48:56 +0500

    Beka

--- lib/pages/Authentication/register_page.dart
+++ lib/pages/Authentication/register_page.dart
@@ -6,7 +6,7 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.red,
     );
   }
 }
